4b3928a3ddc941da36e09cf8ee4a8815b2ca74ef
hardodehack <erhardik.cse@gmail.com>
2016-09-14 00:03:09 +0530

    Added Eclipse workspace

---
+++ Eclipse/PracticeProject1/src/com/hardik/main/DeadlockInThread.java
@@ -0,0 +1,13 @@
+package com.hardik.main;
+
+public class DeadlockInThread extends Thread {
+	
+
+	public static void main(String[] args) {
+
+		DeadlockInThread dl1 = new DeadlockInThread();
+		DeadlockInThread dl2 = new DeadlockInThread();
+		
+	}
+
+}
